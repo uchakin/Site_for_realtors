538a4a8c49f0a384026020eba51be8ef48745b79
400notOK <50711155+400notOK@users.noreply.github.com>
2020-05-01 15:03:25 +0300

    Add field Flat.new_building

--- property/models.py
+++ property/models.py
@@ -6,6 +6,8 @@ class Flat(models.Model):
     owner = models.CharField("ФИО владельца", max_length=200)
     owners_phonenumber = models.CharField("Номер владельца", max_length=20)
     created_at = models.DateTimeField("Когда создано объявление", default=timezone.now, db_index=True)
+
+    new_building = models.NullBooleanField(null=True)
     
     description = models.TextField("Текст объявления", blank=True)
     price = models.IntegerField("Цена квартиры", db_index=True)
