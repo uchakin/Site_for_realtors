1ec9501a072878ae9f53baab755c667995adb887
400notOK <50711155+400notOK@users.noreply.github.com>
2020-05-01 14:25:43 +0300

    Initial commit
    Customize Flat Admin

---
+++ property/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Flat
+
+
+class FlatAdmin(admin.ModelAdmin):
+    search_fields = ('owner', 'town', 'address')
+
+
+admin.site.register(Flat, FlatAdmin)
