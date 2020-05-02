866d0ea659eba9f0ab9c830f093d4a45ad0e4c22
400notOK <50711155+400notOK@users.noreply.github.com>
2020-05-02 15:07:33 +0300

    Normalize Flat.phonenumbers into Flat.owner_phone_pure

--- property/admin.py
+++ property/admin.py
@@ -6,7 +6,8 @@ from .models import Flat, Report
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('owner', 'town', 'address')
     readonly_fields = ['created_at']
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
+    list_display = (
+        'address', 'price', 'new_building', 'construction_year', 'town', 'owners_phonenumber', 'owner_phone_pure')
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     raw_id_fields = ('liked_by',)
--- property/models.py
+++ property/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
     owner = models.CharField("ФИО владельца", max_length=200)
     owners_phonenumber = models.CharField("Номер владельца", max_length=20)
+    owner_phone_pure = PhoneNumberField(
+        blank=True, region="RU", verbose_name="Нормализованный номер владельца")
     created_at = models.DateTimeField("Когда создано объявление", default=timezone.now, db_index=True)
 
     new_building = models.NullBooleanField(null=True)
